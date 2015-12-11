029497458cdba006cec4488331ee622ffc409467
jmdelvalle <jeremy.delvalle@flatironschool.com>
2015-12-11 14:25:01 -0500

    callback

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '465426436979059', 'ca05decee4b41e7e1f690e03f098d7e4', {:callback_url => 'https://fithub-release.herokuapp.com//auth/facebook/callback/'}
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], {:callback_url => 'https://fithub-release.herokuapp.com//auth/facebook/callback/'}
 end

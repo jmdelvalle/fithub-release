c26b1d88beb67716fc714b1a5354183ade6d3996
jmdelvalle <jeremy.delvalle@flatironschool.com>
2015-12-11 14:36:18 -0500

    .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require bootstrap
+//= require twitter/bootstrap
 //= require_tree ../../../vendor/assets/javascripts/.
 //= require_tree .
